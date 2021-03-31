5e2b9ae641526439232073a76fda773aeeafa859
fritzgt <fritzgt@icloud.com>
2021-03-31 23:43:50 +0300

    Complete thirdView functionality

--- WeVidIt Project/View Controllers/ThirdView.swift
+++ WeVidIt Project/View Controllers/ThirdView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ThirdView: View {
-    var finalSelection: City?
+    //MARK: - Properties
+    @State private var input: String = ""
+    var finalSelection: City? {
+        didSet{
+            input = finalSelection!.cityName.rawValue
+        }
+    }
+    @State var entryArray: [String] = [""]
     
+    
+    //MARK: - body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        HStack{
+            VStack{
+                TextField(input, text: $input)
+                    .font(Font.system(size: 24, design: .default))
+                    .padding(.leading, 10)
+                Divider()
+            }
+            Button("+ Add") {
+                //Create the action here
+                if input != ""{
+                    entryArray.append(input)
+                    input = ""//Clear field
+                }
+            }.frame(width: 80, height: 40, alignment: .center).overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.black, lineWidth: 2)
+            ).padding()
+        }
+        List {
+            ForEach(0..<entryArray.count, id: \.self){ index in
+                if entryArray[index] != ""{
+                    HStack{
+                        Text(entryArray[index])
+                        Button("X") {
+                            entryArray.remove(at: index)
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 }
 
@@ -20,3 +60,4 @@ struct ThirdView_Previews: PreviewProvider {
         ThirdView()
     }
 }
+
