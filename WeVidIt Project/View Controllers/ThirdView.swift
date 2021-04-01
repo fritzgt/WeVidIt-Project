a58b6fd90a6d58360b1bc0b69c6e9b2fbdc41759
fritzgt <fritzgt@icloud.com>
2021-04-01 10:46:04 +0300

    completed third view ui

--- WeVidIt Project/View Controllers/ThirdView.swift
+++ WeVidIt Project/View Controllers/ThirdView.swift
@@ -20,45 +20,64 @@ struct ThirdView: View {
     
     init(finalSelection: City){
         self.finalSelection = finalSelection
+        //Use to set the inital value for the textfield
         self._input = State(wrappedValue: finalSelection.cityName.rawValue)
     }
-   
+    
     //MARK: - body
     var body: some View {
         
+        
         HStack{
             VStack{
-                
-                    
-                    TextField(input, text: $input)
-                        .font(Font.system(size: 24, design: .default))
-                        .padding(.leading, 10)
-                    Divider()
+                TextField(input, text: $input)
+                    .font(Font.system(size: 24, design: .default))
+                    .padding(.leading, 10)
+                Divider().frame(height: 2).background(Color.black)
                 
             }
-            Button("+ Add") {
-                //Create the action here
+            Button("➕ Add") {
                 if input != ""{
                     entryArray.append(input)
-                    input = ""//Clear field
+                    input = ""//Clear textField
                 }
-            }.frame(width: 80, height: 40, alignment: .center).overlay(
+            }.foregroundColor(.black).frame(width: 80, height: 40, alignment: .center).overlay(
                 RoundedRectangle(cornerRadius: 5)
                     .stroke(Color.black, lineWidth: 2)
             ).padding()
         }
-        List {
-            ForEach(0..<entryArray.count, id: \.self){ index in
-                if entryArray[index] != ""{
-                    HStack{
-                        Text(entryArray[index])
-                        Button("X") {
-                            entryArray.remove(at: index)
-                        }
+        ScrollView(.horizontal, content: {
+            HStack(spacing: 15) {
+                ForEach(0..<entryArray.count, id: \.self){ index in
+                    if entryArray[index] != ""{
+                        
+                        Button(action: {
+                            
+                        }) {
+                            HStack{
+                                Text(entryArray[index]).font(Font.system(size: 20, design: .default))
+                                Button("X") {
+                                    entryArray.remove(at: index)
+                                }.frame(width: 30, height: 30)
+                                .foregroundColor(Color("customPurple"))
+                                .background(Color.black)
+                                .clipShape(Circle())
+                                .font(Font.system(size: 20, design: .default).bold())
+                            }
+                        }.padding(10)
+                        .background(Color("customPurple"))
+                        .cornerRadius(40)
+                        .foregroundColor(.black)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color("customPurple"), lineWidth: 5)
+                        )
                     }
                 }
-            }
+            }.padding()
         }
+        )
+        Spacer()
     }
 }
 
